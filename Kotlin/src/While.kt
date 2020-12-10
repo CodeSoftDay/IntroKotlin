037964aa0940a35f8a1056492dd05774fb8c4992
manuelduarte077 <carlosd99m@gmail.com>
2020-12-10 14:23:23 -0600

    Uso del (while y el do while)

---
+++ Kotlin/src/While.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+
+    // < = Menor
+    // > = Mayor
+
+    var x = 0
+
+    while ( x <= 10 ) {
+        println( x )
+        x++
+    }
+
+    var y = 0
+
+    do {
+        println( x )
+        y++
+    } while ( y <= 20 )
+
+}
