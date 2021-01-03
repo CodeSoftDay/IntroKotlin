7bbcac24d70cd37e5dddb29fed750b074865784d
manuelduarte077 <carlosd99m@gmail.com>
2021-01-02 21:30:41 -0600

    Ciclo For

---
+++ CursoKT/src/seccion5/CicloFor.kt
@@ -0,0 +1,16 @@
+package seccion5
+
+fun main(args: Array<String>) {
+
+    // Ciclo For
+
+    for (elemento in 1..5) {
+        println("El valor es $elemento")
+
+        if (elemento == 3) {
+            println("Elemento econtrado $elemento")
+        }
+
+    }
+
+}
