975c7a18dd556d78a4a0e7caaaa566f31b6f4ff2
Manuel <carlosd99m@gmail.com>
2021-03-10 22:06:14 -0600

    Constantes

---
+++ exampleKotlin/src/Constantes.kt
@@ -0,0 +1,12 @@
+
+const val cedula = "607-010601"
+
+fun main(args:  Array<String>){
+    println(Constantes.cedula)
+}
+
+class Constantes{
+    companion object{
+        const val cedula = "121-020356"
+    }
+}
