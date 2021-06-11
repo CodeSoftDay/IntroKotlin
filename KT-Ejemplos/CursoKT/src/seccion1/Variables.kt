546a19b8ef4d188ad8c1c2aa00931c78faf529dc
Manuel Duarte <carlosd99m@gmail.com>
2021-06-11 17:56:53 -0600

    fix  :fire:

--- KT-Ejemplos/CursoKT/src/seccion1/Variables.kt
+++ KT-Ejemplos/CursoKT/src/seccion1/Variables.kt
@@ -5,7 +5,7 @@ fun main (args: Array<String>) {
 
     // Variables Numericas
     val x : Int = 2
-    val y : Int = 6
+    val y : Int = 5
     val z : Int = 8
 
     println("El valor de x es: $x")
@@ -21,7 +21,7 @@ fun main (args: Array<String>) {
     println("Nombre $name, edad $edad, ciudad $country")
 
     var flotante : Float = 25.127f
-    var doble = 25.127343456
+    var double 25.127343456
 
 
 }
